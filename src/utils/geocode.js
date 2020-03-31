c96929ea52a88f441840cc55eeb9f44d0c68d5c9
umair akram <amianumair@gmail.com>
2020-03-31 22:53:18 +0500

    Updated Repositories

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,4 +1,3 @@
-// const request_promice=require('request-promise')
 const axios=require('axios')
 const geocode=function(address,callback){
     const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidW1haXJha3JhbSIsImEiOiJjazhhYmpncWowMG9hM3BxdGp1eWdpODBhIn0.AXEwReXgQMv8NzIRd0S5RQ&limit=1'
@@ -17,24 +16,6 @@ const geocode=function(address,callback){
             callback("Unable to connect with location services"+error,undefined)
     })    
 }
-
-
-// const geocode=function(address,callback){
-//     const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidW1haXJha3JhbSIsImEiOiJjazhhYmpncWowMG9hM3BxdGp1eWdpODBhIn0.AXEwReXgQMv8NzIRd0S5RQ&limit=1'
-//     request_promice({url,json:true},(error,{body})=>{
-//         if(error){
-//             callback("Unable to connect with location services",undefined)
-//         }else if(body.features.length===0){
-//             callback('Unable to find the Location,Try another search',undefined)
-//         }else{
-//             callback(undefined,{
-//                 longitude:body.features[0].center[0],
-//                 latitude:body.features[0].center[1],
-//                 location:body.features[0].place_name                
-//             })
-//         }
-//     })    
-// }
 
 
 
--- src/utils/weatherForcast.js
+++ src/utils/weatherForcast.js
@@ -1,4 +1,3 @@
-// const request_promice=require('request-promise')
 const axios=require('axios')
 const weatherForcast=(latitude,longitude,callback)=>{
     const url='https://api.darksky.net/forecast/b3b186b490908e1faef41d4b072e8709/'+latitude+','+longitude+'?units:si&exclude:hourly,minutely'
@@ -19,20 +18,6 @@ const weatherForcast=(latitude,longitude,callback)=>{
 
 
 
-// const weatherForcast=(latitude,longitude,callback)=>{
-//     const url='https://api.darksky.net/forecast/b3b186b490908e1faef41d4b072e8709/'+latitude+','+longitude+'?units:si&exclude:hourly,minutely'
-//     request_promice({url, json:true },(error,{body})=>{
-//         if(error){
-//             callback("Unable to connnect with weather services",undefined)
-//         }else if(body.error){
-//             callback("Unable to find Location",undefined)
-//         }else{
-//             callback(undefined,
-//                 body.daily.data[0].summary + 'It is currently '+
-//                 body.currently.temperature + ' degress out. There is a '+
-//                 body.currently.precipProbability +'% chance of rain')  
-//         }
-//     })
-// }
+
 
 module.exports=weatherForcast
