d4f78dcf48ff77d814ed38bc5e72bf6bb363f69e
umair akram <amianumair@gmail.com>
2020-03-31 12:18:59 +0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 console.log("This is the text message to check the js ");
-// const fetch =require('node-fetch')
 
 
 
@@ -16,7 +15,7 @@ weatherForm.addEventListener('submit',(e)=>{
     const location =search.value;
     messageOne.textContent='Loading ...'
     messageTwo.textContent=''
-    fetch('http://localhost:3000/weather?address='+location+'').then((response)=>{
+    fetch('/weather?address='+location+'').then((response)=>{
         response.json().then((data={})=>{
             if(data.error){
                 messageOne.textContent=data.error
--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ console.log(publicDirectoryPath)
 const pathViews=path.join(__dirname,'../templates/views')
 const partialsPath=path.join(__dirname,'../templates/partials')
 const app=express()
-
+const port=process.env.PORT||3000
 app.use(express.static(publicDirectoryPath))
 app.set("view engine",'hbs')
 app.set('views',pathViews)
@@ -94,8 +94,8 @@ app.get('*',(req,res)=>{
 // })
 
 
-app.listen(3000,()=>{
-    console.log("Server is running on port:3000")
+app.listen(port,()=>{
+    console.log("Server is running on port:"+port)
 })
 
 
